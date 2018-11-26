346e94cc8eced1b07d62c58fad942fc38a79461e
Riffer10000 <35526854+Riffer10000@users.noreply.github.com>
2018-11-26 21:06:39 +0100

    Final version
    Final upload of the Atmel Studio project

---
+++ C/Project 2.1/Project 2.1/Debug/main.d
@@ -0,0 +1,54 @@
+main.d main.o: .././main.c \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ E:\School\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom328p.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
+ .././AVR_TTC_scheduler.c .././AVR_TTC_scheduler.h \
+ e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+E:\School\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom328p.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+.././AVR_TTC_scheduler.c:
+
+.././AVR_TTC_scheduler.h:
+
+e:\school\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
